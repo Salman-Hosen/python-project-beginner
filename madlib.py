fd58b17c8cd2eeea05a8ddcb74fe2dfc266e4d30
Salman <94845268+Salman-Hosen@users.noreply.github.com>
2024-10-05 14:52:36 +0600

    initial

---
+++ madlib.py
@@ -0,0 +1,12 @@
+adjective1 = input("Adjective: ")
+noun1 = input("Noun: ")
+verb1 = input("Verb: ")
+adjective2 = input("Adjective : ")
+noun2 = input("Noun : ")
+verb2 = input("Verb: ")
+adjective3 = input("Adjective : ")
+
+story = f"The {adjective1} {noun1} began to {verb1} through the crowded street. A {adjective2} {noun2} quickly followed, \
+trying to {verb2} faster. Together, they created a {adjective3} scene that confused everyone nearby."
+
+print(story)
